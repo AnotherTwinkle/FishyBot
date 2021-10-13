ac6f4c4f909c64f9375ed18c78b636fa47a854c0
S.M.A Nahian <smanahian123@gmail.com>
2021-10-13 19:44:19 +0600

    Set up main launcher

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from bot import FishyBot
+from dotenv import load_dotenv
 
+def main():
+    load_dotenv()
+    client = FishyBot("!")
+    client.set_up()
+    client.run_bot()
+
+
+if __name__ == "__main__":
+    main()
